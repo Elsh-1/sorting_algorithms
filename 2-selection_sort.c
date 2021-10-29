59a12705ace2ccfa38582ebb9622bb0fdfc83801
root <e.abiynur@gmail.com>
2021-10-29 01:02:55 -0700

    sorting

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,24 +6,24 @@
   */
 void selection_sort(int *array, size_t size)
 {
-	size_t x, x2;
+	size_t i, i2;
 	int min, tmp, idx;
 
-	for (x = 0; x < size; x++)
+	for (i = 0; i < size; i++)
 	{
 		min = array[i];
-		for (x2 = x + 1; x2 < size; x2++)
+		for (i2 = i + 1; i2 < size; i2++)
 		{
-			if (min > array[x2])
+			if (min > array[i2])
 			{
-				min = array[x2];
-				idx = x2;
+				min = array[i2];
+				idx = i2;
 			}
 		}
-		if (min != array[x])
+		if (min != array[i])
 		{
-			tmp = array[x];
-			array[x] = min;
+			tmp = array[i];
+			array[i] = min;
 			array[idx] = tmp;
 			print_array(array, size);
 		}
